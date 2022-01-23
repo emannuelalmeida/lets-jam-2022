8378104efdf0e9b6cd60770b2a911a548e338013
Herbert Weigert Venancio <hwvenancio@gmail.com>
2022-01-23 10:59:06 -0300

    refactored jump

--- boomer-vs-zoomer/scenes/Player.gd
+++ boomer-vs-zoomer/scenes/Player.gd
@@ -1,19 +1,24 @@
 extends KinematicBody
 
-export var speed = 14
-export var fall_acceleration = 75
+const jump_speed = 10
+const speed = 14
 
 var velocity = Vector3.ZERO
+onready var gravityDir = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
+onready var gravityVal = ProjectSettings.get_setting("physics/3d/default_gravity")
 onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 
 var is_jumping = false
-var vertical_velocity = Vector3()
+var vertical_velocity = 0.0
+
 
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
-	vertical_velocity = gravity * delta
-	var jump_speed = 100
+	if is_jumping:
+		vertical_velocity += gravityVal * delta
+	else:
+		vertical_velocity = 0
 
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
@@ -21,14 +26,12 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_down"):
-		# Notice how we are working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
 	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
 	
 	if Input.is_action_pressed("jump") and not is_jumping:
-		direction.y = 1
+		vertical_velocity = jump_speed
 		is_jumping = true
 	
 	if direction != Vector3.ZERO:
@@ -37,12 +40,11 @@ func _physics_process(delta):
 	
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
-	velocity.y = direction.y * jump_speed
-	# Vertical velocity
-	velocity.y -= fall_acceleration * delta
+	velocity.y = vertical_velocity
 	# Moving the character
-	velocity = move_and_slide(velocity, -gravity.normalized())
+	velocity = move_and_slide(velocity, -gravityDir)
 	
 	if is_on_floor():
+		direction.y = 0.0
 		is_jumping = false
 	
