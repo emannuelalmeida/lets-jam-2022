1c85109a6dda4deb2f0541a0b5330eb9303ea600
Ícaro Emannuel <emannuelalmeida@gmail.com>
2022-01-30 16:13:07 -0300

    HUD added

--- boomer-vs-zoomer/scenes/Stage.gd
+++ boomer-vs-zoomer/scenes/Stage.gd
@@ -4,6 +4,14 @@ var areas_total = 3
 var current_area = 1
 var area_size = 8
 
+func _process(delta):
+	if $"../HUD/EnemyHealthBar".value <= 0:
+		$"../HUD/EnemyHealthBar".modulate.a -= 1 * delta
+		$"../HUD/EnemyName".modulate.a -= 1 * delta
+	else:
+		$"../HUD/EnemyHealthBar".modulate.a = 1
+		$"../HUD/EnemyName".modulate.a = 1
+
 func _move_camera_to_next_scene():
 		var camera = get_node("../CameraPivot/Camera")
 		if (current_area == areas_total):
@@ -16,9 +24,19 @@ func _move_camera_to_next_scene():
 
 func _on_Player_TriggerGameOver():
 	get_tree().change_scene("res://scenes/GameOver.tscn")
+<<<<<<< HEAD
 
 
 func _on_EndStage_body_entered(body):
 	if(body.is_in_group("Player")):
 		get_tree().change_scene("res://scenes/GameOver.tscn")
 	pass # Replace with function body.
+=======
+	
+func _on_Player_TriggerPlayerHudUpdate(current_life, max_life):
+	$"../HUD/PlayerHealthBar".value = current_life
+	
+func _on_Enemy_TriggerEnemyHudUpdate(name, life):
+	$"../HUD/EnemyName".text = name
+	$"../HUD/EnemyHealthBar".value = life
+>>>>>>> 64d652a (HUD added)
