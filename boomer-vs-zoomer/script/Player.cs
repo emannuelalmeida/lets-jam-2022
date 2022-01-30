e4595abf0a093b171b580b0959a3d8072050df52
marcostbsoares <marcostbsoares@gmail.com>
2022-01-30 12:16:52 -0300

    New enemy and player (#16)
    * Added new game with simple mechanics, with collisions and jump.
    
    * refactored jump
    
    * Criada projeção de camera, movimentação em cenário e sprite 3D animado do personagem
    
    * Camera movement added for the stage (#9)
    
    * Generic state machine and initial enemy states
    
    * Implemented remaining enemy states
    
    * Second enemy implemented
    
    * Menu and scene and opening menu as default scene
    
    * Implemented player
    
    Co-authored-by: Ícaro Emannuel <emannuelalmeida@gmail.com>
    Co-authored-by: Herbert Weigert Venancio <hwvenancio@gmail.com>

---
+++ boomer-vs-zoomer/script/Player.cs
@@ -0,0 +1,196 @@
+using Godot;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Player : KinematicBody
+{
+    enum PlayerStates
+    {
+        IDLE,
+        WALK,
+        JUMP_UP,
+        JUMP_FALL,
+        ATTACK_1,
+        ATTACK_RNG,
+        HURT,
+        DYING
+    }
+
+    //Node references
+    AnimatedSprite3D animation;
+    Spatial pivot;
+    Area attackHitbox;
+    //
+
+    //Parameters
+    float movementSpeed = 600f;
+    int maxHealth = 100;
+    int currentHealth = 100;
+    int attackDamage = 10;
+    //
+
+    StateMachine<PlayerStates> stateMachine;
+    Vector3 translate = Vector3.Zero;
+    float delta;
+
+    bool invulnerable = false;
+    bool dead = false;
+    bool canHit = true;
+
+    public override void _Ready()
+    {
+        animation = GetNode<AnimatedSprite3D>("Pivot/Animation");
+        pivot = GetNode<Spatial>("Pivot");
+        attackHitbox = GetNode<Area>("Pivot/AttackHitbox");
+
+        stateMachine = new StateMachine<PlayerStates>(animation);
+        stateMachine.AddState(PlayerStates.IDLE, "idle", null, () => UpdateIdle());
+        stateMachine.AddState(PlayerStates.WALK, "walk", null, () => UpdateMovement());
+        stateMachine.AddState(PlayerStates.ATTACK_1, "attack", () => OnEnterAttack(), () => UpdateAttack());
+        stateMachine.AddState(PlayerStates.HURT, "hurt", null, () => UpdateHurt());
+        stateMachine.AddState(PlayerStates.DYING, "dead", null, () => UpdateDying());
+
+        animation.Connect("animation_finished", this, "OnAnimationFinished");
+    }
+    
+    public override void _Process(float delta)
+    {
+        translate = Vector3.Zero;
+        this.delta = delta;
+
+        stateMachine.UpdateCurrentState();
+
+        if(translate.Length() > 0.01f)
+            MoveAndSlide(translate);
+
+        if(translate.x > 0.01f)
+            pivot.RotationDegrees = new Vector3(0, 0, 0);
+
+        else if(translate.x < -0.01f)
+            pivot.RotationDegrees = new Vector3(0, 180, 0);
+    }
+
+    //States
+    private void UpdateIdle() 
+    {
+        if(VerifyStartAttack())
+            return;
+
+        VerifyMovement();
+
+        if(translate.Length() > 0.01f)
+            stateMachine.ChangeState(PlayerStates.WALK);
+    }
+
+    private void UpdateMovement()
+    {
+        if(VerifyStartAttack())
+            return;
+
+        VerifyMovement();
+
+        if(translate.Length() < 0.01f)
+            stateMachine.ChangeState(PlayerStates.IDLE);
+    }
+
+    private void OnEnterAttack()
+    {
+        canHit = true;
+    }
+
+    private void UpdateAttack()
+    {
+        if(canHit && animation.Frame == 3)
+        {
+            canHit = false;
+            List<Node> enemies = attackHitbox.GetOverlappingBodies().Cast<Node>().Where(x => x.IsInGroup("Enemy")).ToList();
+            GD.Print($"Enemies: {enemies.Count}");
+            enemies.ForEach(x => {
+                x.Call("HurtEnemy", attackDamage);
+            });
+        }
+    }
+
+    private void UpdateHurt()
+    {
+
+    }
+
+    private void UpdateDying()
+    {
+        if(dead)
+            animation.Opacity -= 1f * delta;
+
+        if(animation.Opacity <= 0f)
+            EmitSignal("TriggerGameOver");
+    }
+
+    //Behaviors
+    private void VerifyMovement()
+    {
+        Vector3 moveDirection = new Vector3();
+
+        if(Input.IsActionPressed("move_up"))
+            moveDirection.z -= 1;
+        if(Input.IsActionPressed("move_down"))
+            moveDirection.z += 1;
+        if(Input.IsActionPressed("move_left"))
+            moveDirection.x -= 1;
+        if(Input.IsActionPressed("move_right"))
+            moveDirection.x += 1;
+
+        translate += moveDirection.Normalized() * movementSpeed * delta;
+    }
+
+    private bool VerifyStartAttack()
+    {
+        if(Input.IsActionJustPressed("attack"))
+        {
+            stateMachine.ChangeState(PlayerStates.ATTACK_1);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void OnAnimationFinished()
+    {
+        switch(animation.Animation)
+        {
+            case "attack":
+                stateMachine.ChangeState(PlayerStates.IDLE);
+                break;
+            case "hurt":
+                invulnerable = false;
+                stateMachine.ChangeState(PlayerStates.IDLE);
+                break;
+            case "dead":
+                dead = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void HitEnemy()
+    {
+
+    }
+
+    public void Hurt(int damage)
+    {
+        invulnerable = true;
+
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            stateMachine.ChangeState(PlayerStates.DYING);
+        }
+        else
+            stateMachine.ChangeState(PlayerStates.HURT);
+    }
+
+    [Signal]
+    public delegate void TriggerGameOver();
+}
